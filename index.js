e94c44a7b09dfcc8757584a0578bd5fef084f723
Sherry Chen <a00942632@se14l0121-sp12.ad.bcit.ca>
2018-03-16 15:37:40 -0700

    add

---
+++ index.js
@@ -0,0 +1,49 @@
+const port = process.env.PORT || 10022;
+const server = require("http").Server();
+
+var io = require("socket.io")(server);
+
+var allqs = {};
+
+io.on("connection", function(socket){
+    
+    socket.on("joinroom", function(data){
+        console.log('joining room', data);
+        
+        socket.join(data);
+        socket.myRoom = data;
+        
+        if(!allqs[data]){
+            allqs[data] = {
+                qobj:{}
+            }
+        }
+    })
+    
+    socket.on('answer', function(data){
+        var msg = 'WRONG!';
+        if(data == allqs[socket.myRoom].qobj.a){
+            msg = "You've got it";
+        }
+        
+        socket.emit('result', msg);
+    })
+    
+    socket.on("qsumbit", function(data){
+        allqs[socket.myRoom].qobj = data;
+        socket.to(socket.myRoom).emit("newq", data);
+    })
+    
+    socket.on("disconnect", function(){
+      
+    })
+})
+
+server.listen(port,(err)=>{
+    if(err){
+        console.log(err);
+        return false;
+    }
+    
+    console.log("Port is runnning");
+})
